df49ca57c86bb07af241abb3470c1ab9718a2b48
SydneyEwenss <sydneyewens06@gmail.com>
2025-04-25 00:32:23 +0100

    fixed some formatting issues and added commands to the skin stealer

--- pages/utils/client/skin-stealer/script.js
+++ pages/utils/client/skin-stealer/script.js
@@ -13,9 +13,44 @@ document.getElementById('skin-form').addEventListener('submit', function(event) 
   skinImage.alt = `Skin of ${username}`;
 
   document.getElementById('skin-container').classList = ""
+  document.getElementById('command-list').classList = ""
 
   const downloadUrl = `https://minotar.net/download/${username}`;
   const downloadButton = document.getElementById('download-button');
   downloadButton.style.display = 'inline-block';
   downloadButton.href = downloadUrl;
+
+  const command = `/give @p player_head[profile={name:"${username}"}] 1`;
+  document.getElementById('command-output').textContent = command;
+
+  const command_1_13 = `/give @p minecraft:player_head{SkullOwner:"${username}"} 1`;
+  document.getElementById('command-output-1.13').textContent = command_1_13;
+});
+
+document.getElementById('copy-command').addEventListener('click', () => {
+  const command = document.getElementById('command-output').textContent;
+  
+  if (command) {
+    navigator.clipboard.writeText(command)
+      .then(() => {
+      })
+      .catch(err => {
+        alert('Failed to copy command.');
+        console.error(err);
+      });
+  }
+});
+
+document.getElementById('copy-command-1.13').addEventListener('click', () => {
+  const command = document.getElementById('command-output-1.13').textContent;
+  
+  if (command) {
+    navigator.clipboard.writeText(command)
+      .then(() => {
+      })
+      .catch(err => {
+        alert('Failed to copy command.');
+        console.error(err);
+      });
+  }
 });
