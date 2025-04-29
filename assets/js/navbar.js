3323269d366433c111e1cce961379ef13434bd72
SydneyEwenss <sydneyewens06@gmail.com>
2025-04-29 17:03:29 +0100

    edited some navbar styling

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.add('dark-mode');
 
     const isDarkMode = document.body.classList.contains('dark-mode');
-    document.getElementById('theme-toggle').innerHTML = isDarkMode ? '<i class="fa-solid fa-moon"></i>' : '<i class="fa-solid fa-sun"></i>';
+    document.getElementById('theme-toggle').innerHTML = isDarkMode ? '<a><i class="fa-solid fa-moon"></i></a>' : '<a><i class="fa-solid fa-sun"></i></a>';
 });
   
 document.getElementById('theme-toggle').addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 
     const isDarkMode = document.body.classList.contains('dark-mode');
-    document.getElementById('theme-toggle').innerHTML = isDarkMode ? '<i class="fa-solid fa-moon"></i>' : '<i class="fa-solid fa-sun"></i>';
+    document.getElementById('theme-toggle').innerHTML = isDarkMode ? '<a><i class="fa-solid fa-moon"></i></a>' : '<a><i class="fa-solid fa-sun"></i></a>';
 
     localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
 });
